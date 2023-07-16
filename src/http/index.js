dbda362be5c4300dab6d1fad5e8d26b4e9f5e4eb
AmIgOs434 <104652966+AmIgOs434@users.noreply.github.com>
2023-07-16 22:28:40 +0300

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL:'http://localhost:5000/'
+    baseURL:'http://45.90.34.155:5000'
 })
 
 const $authHost = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'http://45.90.34.155:5000'
 })
 
 
